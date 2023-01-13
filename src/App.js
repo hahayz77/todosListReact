c0c8d8b800256179445ad45410d23b0e2a9e3591
hahayz77 <danilo__danilo@hotmail.com>
2023-01-13 18:47:22 -0300

    custom screem + hidden / d-block

--- src/App.js
+++ src/App.js
@@ -11,31 +11,36 @@ function App() {
 	const { inputValue, inputChange, toDoList, onAdd } = useStateContext();
 
   	return (
-    <div className='mx-auto mt-10 mb-8 py-6 rounded-3xl bg-opacity-90 bg-white lg:max-w-4xl lg:px-6 md:max-w-3xl min-w-xs px-6'>
-		<div className="flex flex-col justify-center max-w-3xl mx-auto">
-		<SvgComponent comp={"done-signal"} />
-			<h1 className='font-bold text-6xl text-center mb-6 text-teal-700'>To Do's</h1>
-			<form action="#" onSubmit={(e)=>{ e.preventDefault() }}>
-				<div className="my-3 grid gap-x-2 grid-cols-12">
-					<div className='sm:col-span-9 col-span-12'>
-						<Input onKeyDown={(e)=>{e.code === "Enter" && onAdd()}} color="teal" label="Input Task" size="lg" value={inputValue}
-						onChange={(e)=>{inputChange(e.target.value)}}
-						/>
-					</div>
-					<div className='sm:col-span-3 col-span-12 place-self-center mt-3 sm:mt-0'>
-						<Button onClick={onAdd} className='inline-flex items-center py-0 px-2 font-black text-teal-800 text-base bg-gradient-to-br from-[#50d18d] to-[#9dffce] hover:bg-gradient-to-bl hover:from-[#50d18d] hover:to-[#9dffce] hover-scale hover:scale-110' size="md" color='teal'>Send Task 
-						<SvgComponent comp={"plus-signal"}/>
-						</Button>
-					</div>
+		<main>
+			<section className='mx-auto mt-10 mb-8 py-6 rounded-3xl bg-opacity-90 bg-white lg:max-w-4xl lg:px-6 md:max-w-3xl min-w-xs px-6'>
+				<div className="flex flex-col justify-center max-w-3xl mx-auto">
+				<SvgComponent comp={"done-signal"} />
+					<h1 className='font-bold text-6xl text-center mb-6 text-teal-700'>To Do's</h1>
+					<form action="#" onSubmit={(e)=>{ e.preventDefault() }}>
+						<div className="my-3 grid gap-x-2 grid-cols-12">
+							<div className='sm:col-span-9 col-span-12'>
+								<Input onKeyDown={(e)=>{e.code === "Enter" && onAdd()}} color="teal" label="Input Task" size="lg" value={inputValue}
+								onChange={(e)=>{inputChange(e.target.value)}}
+								/>
+							</div>
+							<div className='sm:col-span-3 col-span-12 place-self-center mt-3 sm:mt-0'>
+								<Button onClick={onAdd} className='inline-flex items-center py-0 px-2 font-black text-teal-800 text-base bg-gradient-to-br from-[#50d18d] to-[#9dffce] hover:bg-gradient-to-bl hover:from-[#50d18d] hover:to-[#9dffce] hover-scale hover:scale-110' size="md" color='teal'>Send Task 
+								<SvgComponent comp={"plus-signal"}/>
+								</Button>
+							</div>
+						</div>
+					<hr className="mb-6 mx-auto w-12/12 h-0.5 bg-teal-100 rounded border-0 opacity-50"/>
+					</form>
+					<ul>
+						{toDoList.map((e, index)=>{return (<Tasks content={e.content} key={index} id={e.id}/>)})}
+					</ul>
 				</div>
-			<hr className="mb-6 mx-auto w-12/12 h-0.5 bg-teal-100 rounded border-0 opacity-50"/>
-			</form>
-			<ul>
-				{toDoList.map((e, index)=>{return (<Tasks content={e.content} key={index} id={e.id}/>)})}
-			</ul>
-		</div>
-		<Done/>
-	</div>
+				<Done/>
+		</section>
+			<footer className='absolute sm:bottom-0 bottom-[-30px] left-0 right-0 text-center text-[10px] hidden xs:block'>
+					<p>Maded by HaHay. Ver 130123. Copyrights 2023</p>
+			</footer>
+	</main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '300px',
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 });
